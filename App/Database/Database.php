8432966947be637c904b9e42bf329e93d93948a3
Yacamdawi <yacin-hamdaoui@hotmail.com>
2019-05-24 18:58:56 +0200

    Ajout de la création de la table mail pour l'enregistré dans la base de données

---
+++ App/Database/Database.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Database;
+use App\Database\Migrations\CreateMailTable;
+use App\Database\Migrations\CreateNewsletterTable;
+
+class Database
+{
+  /**
+   * Initialisation de la base de donnée. Cette méthode est déclanché lorsque l'on active le plugin.
+   *
+   * @return void
+   */
+  public static function init()
+  {
+    // Création des tables dans la base de donnée.
+    self::migration();
+  }
+  /**
+   * Méthode qui lance les migrations afin que chacune créé sa table dans la base de donnée.
+   *
+   * @return void
+   */
+  public static function migration()
+  {
+    CreateMailTable::up();
+    CreateNewsletterTable::up();
+  }
+}
